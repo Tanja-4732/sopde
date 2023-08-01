f3e36b1c8d8c2b750db2fe19e5638c754bc25013
Tanja <git@tanja.pw>
2023-08-01 19:54:46 +0200

    Single-page version

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { Component, createEffect, createResource, createSignal, lazy, onMount } from "solid-js";
 import { degrees, PDFDocument, PDFPage, rgb, StandardFonts } from 'pdf-lib';
 import { OnProgressParameters, PDFDocumentLoadingTask, PDFDocumentProxy, PDFPageProxy, RenderTask, PageViewport, TextLayerRenderTask, AbortException, AnnotationEditorLayer, AnnotationEditorParamsType, AnnotationEditorType, AnnotationEditorUIManager, AnnotationLayer, AnnotationMode, build, CMapCompressionType, createValidAbsoluteUrl, FeatureTest, getDocument, getFilenameFromUrl, getPdfFilenameFromUrl, getXfaPageViewport, GlobalWorkerOptions, ImageKind, InvalidPDFException, isDataScheme, isPdfFile, loadScript, MissingPDFException, normalizeUnicode, OPS, PasswordResponses, PDFDataRangeTransport, PDFDateString, PDFWorker, PermissionFlag, PixelsPerInch, PromiseCapability, RenderingCancelledException, renderTextLayer, setLayerDimensions, shadow, SVGGraphics, UnexpectedResponseException, updateTextLayer, Util, VerbosityLevel, version, XfaLayer } from "pdfjs-dist";
-// GlobalWorkerOptions.workerSrc = await import("pdfjs-dist/build/pdf.worker.entry"); // HACK this is the recommended workaround for the worker not being found
-// import { pdfjsWorker } from "pdfjs-dist/build/pdf.worker.entry"; // I would prefer this, but it doesn't work (yet)
+// GlobalWorkerOptions.workerSrc = await import("pdfjs-dist/build/pdf.worker.js"); // HACK this is the recommended workaround for the worker not being found
+// import { pdfjsWorker } from "pdfjs-dist/build/pdf.worker.js"; // I would prefer this, but it doesn't work (yet)
 
-import logo from "./assets/logo.svg";
-import styles from "./App.module.css";
+import "pdfjs-dist/build/pdf.worker.entry";
+
+// GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${version}/pdf.worker.js`;
+
 import { RenderParameters } from "pdfjs-dist/types/src/display/api";
 
 const App: Component = () => {
-  import('pdfjs-dist/build/pdf.worker.entry').then((pdfjsWorker) => {
-    console.log("pdfjsWorker", pdfjsWorker);
-
-    return GlobalWorkerOptions.workerSrc = pdfjsWorker;
-  })
-
-
   const [text, setText] = createSignal("Hello world");
   const [xCoord, setXCoord] = createSignal(42);
   const [yCoord, setYCoord] = createSignal(600);
@@ -26,36 +21,46 @@ const App: Component = () => {
   const pdfParams = () => ({ pdf_document: inputPdf()?.arrayBuffer(), x_coord: xCoord(), y_coord: yCoord(), rotation: rotation(), text: text() } as PdfEdit);
 
   const [pdf, { mutate: mutatePdf, refetch: refetchPdf }] = createResource(pdfParams, modifyPdf);
-  // modifyPdf(pdfParams);
-
-  // getPdf().then(setPdfDocument);
-  // const renderTarget = new pdfjs.Document();^
 
   let canvas: HTMLCanvasElement = document.createElement("canvas");
-  let iframe: HTMLIFrameElement = document.createElement("iframe");
+  let ctx: CanvasRenderingContext2D | null = null;
   onMount(() => {
-    const ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d");
+    if (ctx == null) {
+      console.error("context is null");
+      throw new Error("context is null");
+    }
   });
 
-  createEffect(() => {
+  createEffect(async () => {
     const my_pdf = pdf();
 
+    if (my_pdf == null) {
+      console.log("pdf is null");
+      return;
+    }
 
-    console.log("my_pdf for the blob", my_pdf);
+    const doc = await getDocument(my_pdf).promise;
+    const page = await doc.getPage(1);
+    const scale = 1.5;
+    const viewport = page.getViewport({ scale });
 
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
 
-    const blob = new Blob([my_pdf], { type: "application/pdf" });
-    iframe.src = window.URL.createObjectURL(blob);
-    console.log("iframe.src", iframe.src);
-    console.log("blob", blob);
+    const renderContext: RenderParameters = {
+      canvasContext: ctx,
+      viewport: viewport
+    };
 
-
+    console.log("rendering");
+    await page.render(renderContext).promise;
+    console.log("rendered");
   });
 
   return (
     <div
       class="text-white bg-[#282c34] text-center min-h-screen flex flex-col items-center justify-center min-w-fit overflow-auto"
-      style="font-size: calc(10px + 2vmin);"
     >
       <h1 class="font-bold" style="font-size: calc(15px + 2vmin);">
         Simple online PDF document editor
@@ -63,7 +68,8 @@ const App: Component = () => {
       <p>
         Create a text box in a PDF document
       </p>
-      <div class="flex flex-row gap-5">
+      <h2 class="text-2xl font-bold">Preview</h2>
+      <div class="flex flex-row gap-5 items-start">
         <form class="grid grid-cols-2 gap-2 flex-grow min-w-fit" id="controls">
           <label for="text-input">Text content</label>
           <textarea value={text()} name="text-input" id="text-input" cols="30" rows="10" class="text-black"
@@ -101,58 +107,6 @@ const App: Component = () => {
             Select file
           </button>
 
-          <label for="Text">Show preview</label>
-          <button type="submit" id="submit"
-            class="bg-slate-500 rounded-lg w-fit px-3"
-            onclick={async (Event) => {
-              Event.preventDefault();
-              console.log("submit");
-
-
-
-              const my_pdf = pdf();
-              console.log(my_pdf);
-
-              if (my_pdf == null) {
-                console.log("pdf is null");
-                return;
-              }
-
-              const doc = await getDocument(my_pdf).promise;
-              const page = await doc.getPage(1);
-
-
-              // Set the scale of the PDF (optional)
-              const scale = 1.5;
-
-              // Get the viewport
-              const viewport = page.getViewport({ scale });
-
-              // Prepare a canvas element
-              canvas.width = viewport.width;
-              canvas.height = viewport.height;
-              const context = canvas.getContext('2d');
-
-              if (context == null) {
-                console.error("context is null");
-                throw new Error("context is null");
-              }
-
-              const renderContext: RenderParameters = {
-                canvasContext: context,
-                viewport: viewport
-              };
-
-              console.log("rendering");
-              await page.render(renderContext).promise;
-              console.log("rendered");
-
-
-            }}
-          >
-            Submit
-          </button>
-
           <label>Save to disk</label>
           <button type="submit" id="save-to-disk" name="save-to-disk"
             class="bg-slate-500 rounded-lg w-fit px-3"
@@ -181,21 +135,10 @@ const App: Component = () => {
             Download
           </button>
         </form>
-        {
-          // <pre class="max-w-xl overflow-clip">
-          //   {text()}
-          // </pre>
-          // <canvas ref={canvas} width="256" height="256" />
-        }
-        {
-          // Render the pdf document
-        }
         <div class="flex flex-col items-center justify-center ">
           <h2 class="text-2xl font-bold">Preview</h2>
-          <iframe ref={iframe} width="1000px" height="600px" />
-
+          <canvas ref={canvas} width="595.28px" height="841.89px" />
         </div>
-
       </div>
     </div>
   );
@@ -212,10 +155,6 @@ interface PdfEdit {
 export default App;
 
 async function modifyPdf({ pdf_document, x_coord, y_coord, rotation, text }: PdfEdit): Promise<ArrayBuffer> {
-
-
-  console.log("modifyPdf", pdf_document, x_coord, y_coord, text);
-
   let pdfDoc: PDFDocument;
   if (pdf_document == null) {
     console.warn("The pdf_document is null, using a blank document as template");
@@ -225,11 +164,8 @@ async function modifyPdf({ pdf_document, x_coord, y_coord, rotation, text }: Pdf
   }
   else {
     pdf_document = await pdf_document; // HACK this should be awaited by the caller, using the framework
-    console.log("pdf_document is not null");
-    console.info("The final pdf here is", pdf_document);
     pdfDoc = await PDFDocument.load(pdf_document);
   }
-
 
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
