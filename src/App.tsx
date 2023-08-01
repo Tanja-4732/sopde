c4e7a8265d217b1a93bac8f495dd066c33f31f29
Tanja <git@tanja.pw>
2023-08-01 13:29:33 +0200

    `iframe` preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { Component, createResource, createSignal, lazy, onMount } from "solid-js";
+import { Component, createEffect, createResource, createSignal, lazy, onMount } from "solid-js";
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import { OnProgressParameters, PDFDocumentLoadingTask, PDFDocumentProxy, PDFPageProxy, RenderTask, PageViewport, TextLayerRenderTask, AbortException, AnnotationEditorLayer, AnnotationEditorParamsType, AnnotationEditorType, AnnotationEditorUIManager, AnnotationLayer, AnnotationMode, build, CMapCompressionType, createValidAbsoluteUrl, FeatureTest, getDocument, getFilenameFromUrl, getPdfFilenameFromUrl, getXfaPageViewport, GlobalWorkerOptions, ImageKind, InvalidPDFException, isDataScheme, isPdfFile, loadScript, MissingPDFException, normalizeUnicode, OPS, PasswordResponses, PDFDataRangeTransport, PDFDateString, PDFWorker, PermissionFlag, PixelsPerInch, PromiseCapability, RenderingCancelledException, renderTextLayer, setLayerDimensions, shadow, SVGGraphics, UnexpectedResponseException, updateTextLayer, Util, VerbosityLevel, version, XfaLayer } from "pdfjs-dist";
+// GlobalWorkerOptions.workerSrc = await import("pdfjs-dist/build/pdf.worker.entry"); // HACK this is the recommended workaround for the worker not being found
+// import { pdfjsWorker } from "pdfjs-dist/build/pdf.worker.entry"; // I would prefer this, but it doesn't work (yet)
 
 import logo from "./assets/logo.svg";
 import styles from "./App.module.css";
 import { RenderParameters } from "pdfjs-dist/types/src/display/api";
 
 const App: Component = () => {
+  import('pdfjs-dist/build/pdf.worker.entry').then((pdfjsWorker) => {
+    console.log("pdfjsWorker", pdfjsWorker);
+
+    return GlobalWorkerOptions.workerSrc = pdfjsWorker;
+  })
+
+
   const [text, setText] = createSignal("");
   const [xCoord, setXCoord] = createSignal(0);
   const [yCoord, setYCoord] = createSignal(0);
@@ -18,39 +27,54 @@ const App: Component = () => {
   // modifyPdf(pdfParams);
 
   // getPdf().then(setPdfDocument);
-  // const renderTarget = new pdfjs.Document();
+  // const renderTarget = new pdfjs.Document();^
 
-  let canvas: HTMLCanvasElement;
+  let canvas: HTMLCanvasElement = document.createElement("canvas");
+  let iframe: HTMLIFrameElement = document.createElement("iframe");
   onMount(() => {
-    canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
   });
 
+  createEffect(() => {
+    const my_pdf = pdf();
+
+
+    console.log("my_pdf for the blob", my_pdf);
+
+
+    const blob = new Blob([my_pdf], { type: "application/pdf" });
+    iframe.src = window.URL.createObjectURL(blob);
+    console.log("iframe.src", iframe.src);
+    console.log("blob", blob);
+
+
+  });
 
   return (
-    <div class="text-white bg-[#282c34] text-center">
-      <header class={styles.header}>
-        { // <img src={logo} class={styles.logo} alt="logo" />
-        }
-        <h1 class="text-3xl font-bold">
-          Simple online PDF document editor
-        </h1>
-        <p>
-          Create a text box in a PDF document
-        </p>
-        <form class="grid grid-cols-2 gap-2">
-          <label for="Text">Text content</label>
-          <textarea value={text()} name="text" id="text-input" cols="30" rows="10" class="text-black"
+    <div
+      class="text-white bg-[#282c34] text-center min-h-screen flex flex-col items-center justify-center min-w-fit overflow-auto"
+      style="font-size: calc(10px + 2vmin);"
+    >
+      <h1 class="text-3xl font-bold">
+        Simple online PDF document editor
+      </h1>
+      <p>
+        Create a text box in a PDF document
+      </p>
+      <div class="flex flex-row gap-5">
+        <form class="grid grid-cols-2 gap-2 flex-grow min-w-fit" id="controls">
+          <label for="text-input">Text content</label>
+          <textarea value={text()} name="text-input" id="text-input" cols="30" rows="10" class="text-black"
             onInput={(Event) => setText(Event.currentTarget.value)}
           />
 
           <label for="x-coord">x coordinate (in pt)</label>
           <input class="text-black" value={xCoord()} name="x-coord" id="x-coord" type="number" onInput={(Event) => setXCoord(Event.currentTarget.valueAsNumber)} />
 
-          <label for="Text">y coordinate (in pt)</label>
+          <label for="y-coord">y coordinate (in pt)</label>
           <input class="text-black" value={yCoord()} name="y-coord" id="y-coord" type="number" onInput={(Event) => setYCoord(Event.currentTarget.valueAsNumber)} />
 
-          <label for="Text">Create textfield</label>
+          <label for="Text">Show preview</label>
           <button type="submit" id="submit"
             class="bg-slate-500 rounded-lg w-fit px-3"
             onclick={async (Event) => {
@@ -68,7 +92,7 @@ const App: Component = () => {
               }
 
               const doc = await getDocument(my_pdf).promise;
-              const page = await doc.getPage(0);
+              const page = await doc.getPage(1);
 
 
               // Set the scale of the PDF (optional)
@@ -92,8 +116,9 @@ const App: Component = () => {
                 viewport: viewport
               };
 
+              console.log("rendering");
               await page.render(renderContext).promise;
-
+              console.log("rendered");
 
 
             }}
@@ -129,12 +154,22 @@ const App: Component = () => {
             Download
           </button>
         </form>
+        {
+          // <pre class="max-w-xl overflow-clip">
+          //   {text()}
+          // </pre>
+          // <canvas ref={canvas} width="256" height="256" />
+        }
+        {
+          // Render the pdf document
+        }
+        <div class="flex flex-col items-center justify-center ">
+          <h2 class="text-2xl font-bold">Preview</h2>
+          <iframe ref={iframe} width="1000px" height="600px" />
 
-        <pre class="max-w-xl overflow-clip">
-          {text()}
-        </pre>
-        <canvas ref={canvas} width="256" height="256" />
-      </header>
+        </div>
+
+      </div>
     </div>
   );
 };
