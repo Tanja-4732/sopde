c586f5cdb1baff6fc93eb4a566b6bdef55794b5c
Tanja <git@tanja.pw>
2023-08-03 15:50:01 +0200

    Log license

---
+++ src/licenseNotice.ts
@@ -0,0 +1,23 @@
+// This file contains a license notice, see the full AGPL license in LICENSE.md
+
+// import { shortVersionString } from "./version";
+
+export const licenseNotice = `
+Sopde is free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Sopde is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with Sopde.
+If not, see https://www.gnu.org/licenses/.
+\n`;
+
+export function logLicenseNotice(): void {
+    // console.group(`Sopde ${shortVersionString} license notice`);
+    console.group(`Sopde license notice`);
+    console.log(licenseNotice);
+    console.groupEnd();
+}
